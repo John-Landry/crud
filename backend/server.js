f26f90b4f34282103f7849a3ca7df844aa7fe696
John-Landry <johnlandry33@gmail.com>
2024-12-18 14:24:12 -0500

    x

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
-import express from 'express';
+import express from 'express'
+
+import os
+
+    from dotenv import load_dotenv, dotenv_values
 
 const app = express();
+
+load_dotenv();
+
+
+print(os.getenv(& quot; MY_KEY & quot;));
+
 
 
 app.get('/', (req, res) => {
     res.send("Hi John, server is on.");
 });
-
 
 app.listen(3000, () => {
     console.log("Server is on 3000, John.");
