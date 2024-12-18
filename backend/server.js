3b8823b91059a9cb0ccddcde2a1a2df8c91cd79d
John-Landry <johnlandry33@gmail.com>
2024-12-18 14:01:13 -0500

    x

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,12 @@ import express from 'express';
 
 const app = express();
 
+
+app.get('/', (req, res) => {
+    res.send("Hi John, server is on.");
+});
+
+
 app.listen(3000, () => {
     console.log("Server is on 3000, John.");
 });
